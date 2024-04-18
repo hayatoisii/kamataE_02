cd8b95977f5817c7fe28a85b67d10cfb938e3dd3
hayatoisii <k023g0022@g.neec.ac.jp>
2024-04-18 11:38:35 +0900

    最初

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -20,7 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ゲームウィンドウの作成
 	win = WinApp::GetInstance();
-	win->CreateGameWindow(L"LE2C_02_イシイ_ハヤト_AL3");
+	win->CreateGameWindow(L"LE2C_02_イシイ_ハヤト_ゲームタイトル");
 
 	// DirectX初期化処理
 	dxCommon = DirectXCommon::GetInstance();
